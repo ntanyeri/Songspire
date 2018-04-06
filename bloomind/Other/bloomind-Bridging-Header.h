e77aca4a91a588ceb6546aa5a0ca008f8be8add0
Niyazi Tanyeri <niyazi@atolye15.com>
2018-04-06 18:04:36 +1000

    Update plist and bridging header

---
+++ bloomind/Other/bloomind-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//These import statements let us use the Spotify SDK in Swift
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+
+#import <DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>
+
+@import LNPopupController;
